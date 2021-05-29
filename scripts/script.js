a158c5c117bc8890a68c4350119c4a11ceff629f
LexMac <va388@yandex.ru>
2021-05-29 17:57:57 +0300

    Mesto 1 (Done)

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,44 @@
-let openPopup = document.querySelector('.profile__edit-button');
+//Поиск содержимого:
+//Попап:
 let popup = document.querySelector('.popup');
-let closePopup = document.querySelector('.popup__close-button')
+//Кнопка редактирования:
+let openPopupButton = document.querySelector('.profile__edit-button');
+//Кнопка закрытия:
+let closePopupButton = document.querySelector('.popup__close-button');
+//Форма:
+let form = document.querySelector('.popup__form');
 
-openPopup.addEventListener('click', toggleClass);
+//Поля в профиле
+let username = document.querySelector('.profile__username');
+let profession = document.querySelector('.profile__profession');
 
-closePopup.addEventListener('click', toggleClass);
+//Поля в попапе
+let usernamePopup = document.querySelector('.popup__input-username');
+let professionPopup = document.querySelector('.popup__input-profession')
 
-function toggleClass() {
-    popup.classList.toggle("popup_opened");
-}
+//Открытие попапа
+function openPopup() {
+    popup.classList.add('popup_opened');
+    usernamePopup.value = username.textContent;
+    professionPopup.value = profession.textContent;
+};
+openPopupButton.addEventListener('click', openPopup);
+
+//Закрытие попапа
+function closePopup() {
+    popup.classList.remove('popup_opened');
+};
+closePopupButton.addEventListener('click', closePopup);
+
+
+//Отправка значения формы
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    username.textContent = usernamePopup.value;
+    profession.textContent = professionPopup.value;
+    closePopup();
+};
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+form.addEventListener('submit', formSubmitHandler); 
